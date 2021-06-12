edbf5498d9333a2dd847ba76bba4cbc7f05eb28e
disp1air <hola-shola@mail.ru>
2021-06-12 12:59:07 +0300

    added logic when 'version' is None or is not specified

--- rssparser/get_args.py
+++ rssparser/get_args.py
@@ -5,21 +5,41 @@ def getArgs():
     parser = argparse.ArgumentParser(
         description='Pure Python command-line RSS reader.')
     parser.add_argument(
-        'source', type=str, nargs='?', default=None, help='RSS URL')
+        'source',
+        type=str,
+        nargs='?',
+        default=None,
+        help='RSS URL'
+    )
     parser.add_argument(
         '--version',
-        type=str, nargs='?', const='Version 1.0', help='Print version info')
+        type=str,
+        nargs='?',
+        default='Version 1.0',
+        const='Version 1.0',
+        help='Print version info'
+    )
     parser.add_argument(
         '--json',
         type=bool,
-        nargs='?', const=True, help='Print result as JSON in stdout')
+        nargs='?',
+        const=True,
+        help='Print result as JSON in stdout'
+    )
     parser.add_argument(
         '--limit',
-        type=int, help='Limit news topics if this parameter provided')
+        type=int,
+        help='Limit news topics if this parameter provided'
+    )
     parser.add_argument(
-        '--verbose', type=bool, help='Outputs verbose status messages')
+        '--verbose',
+        type=bool,
+        help='Outputs verbose status messages'
+    )
 
     args = parser.parse_args()
-    # print(args)
+
+    if not args.source:
+        print(args.version)
     # sys.stdout.write(str(args))
     return args
