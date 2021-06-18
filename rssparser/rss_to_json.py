0a3b476f1fad230506753089efd05e79a18e9710
disp1air <hola-shola@mail.ru>
2021-06-18 21:13:30 +0300

    commit

--- rssparser/rss_to_json.py
+++ rssparser/rss_to_json.py
@@ -1,21 +1,33 @@
 import json
+import pickle
 
 
 def rss_to_json(child_exept_items, items):
-    resultDict = {}
+    result_dict = rss_to_dict(child_exept_items, items)
+    store_data(result_dict)
+    json_object = json.dumps(result_dict, indent=4)
+    print(json_object)
+
+
+def rss_to_dict(child_exept_items, items):
+    result = {}
 
     for child in child_exept_items:
         if (child.text and child.text.rstrip()):
-            resultDict[child.tag] = child.text
+            result[child.tag] = child.text
         else:
-            resultDict[child.tag] = {}
+            result[child.tag] = {}
             for i in child:
-                resultDict[child.tag][i.tag] = i.text
+                result[child.tag][i.tag] = i.text
 
     for index, item in enumerate(items):
-        resultDict[f'item{index}'] = {}
+        result[f'item{index}'] = {}
         for i in item:
-            resultDict[f'item{index}'][i.tag] = i.text if i.text else ''
+            result[f'item{index}'][i.tag] = i.text if i.text else ''
 
-    json_object = json.dumps(resultDict, indent=4)
-    print(json_object)
+    return result
+
+
+def store_data(data):
+    with open("dict.pickle", "wb") as file:
+        pickle.dump(data, file)
