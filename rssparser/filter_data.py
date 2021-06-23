a1179884c758faea2163e2404630b7a51e3ce4f0
disp1air <hola-shola@mail.ru>
2021-06-23 20:36:44 +0300

    added several formats

--- rssparser/filter_data.py
+++ rssparser/filter_data.py
@@ -6,11 +6,7 @@ def filter_by_date(list, requested_date):
 
     for item in list:
         if isinstance(item, dict) and item.get('pubDate'):
-            date_obj = datetime.strptime(
-                item.get('pubDate'),
-                # '%a, %d %b %Y %H:%M:%S %z'
-                '%Y-%m-%dT%H:%M:%SZ'
-            )
+            date_obj = find_format(item.get('pubDate'))
 
             if requested_date == date_obj.date().strftime('%Y%m%d'):
                 result.append(item)
@@ -20,3 +16,12 @@ def filter_by_date(list, requested_date):
 
 def filter_by_limit(list, limit):
     return list[:limit]
+
+
+def find_format(text):
+    for fmt in ('%Y-%m-%dT%H:%M:%SZ', '%a, %d %b %Y %H:%M:%S %z', '%a, %d %b %Y %H:%M EDT'):
+        try:
+            return datetime.strptime(text, fmt)
+        except ValueError:
+            pass
+    raise ValueError('no valid date format found')
